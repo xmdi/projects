560110cbee39ff98d129f96c0a97365459a1edfa
xmdi <xmdi@protonmail.com>
2024-12-27 21:14:57 -0500

    reformat and adjust bgcolor

--- goodcad/index.js
+++ goodcad/index.js
@@ -5,106 +5,103 @@ canvas.height=window.innerHeight;
 pureGL(canvas);
 
 function parse() {
-  var information = document.getElementById("params");
-  var data = JSON.parse(information.value);
-  for (var obj in data) {
-    console.log(data[obj].name + " at ID#" + data[obj].id + " is " + data[obj].value);
-  }
+	var information = document.getElementById("params");
+	var data = JSON.parse(information.value);
+	for (var obj in data) {
+		console.log(data[obj].name + " at ID#" + data[obj].id + " is " + data[obj].value);
+	}
 
- information.value=JSON.stringify(JSON.parse(information.value),null,2);
+	information.value=JSON.stringify(JSON.parse(information.value),null,2);
 
 }
 
 
-//pureGL.js
-
 var vertices = [], indices = [], nVertices = 0, gl;
-
 function perspective(fovy, aspect, near, far) {
 	var out=[]
-    var f = 1.0 / Math.tan(fovy / 2),
-        nf;
-    out[0] = f / aspect;
-    out[1] = 0;
-    out[2] = 0;
-    out[3] = 0;
-    out[4] = 0;
-    out[5] = f;
-    out[6] = 0;
-    out[7] = 0;
-    out[8] = 0;
-    out[9] = 0;
-    out[11] = -1;
-    out[12] = 0;
-    out[13] = 0;
-    out[15] = 0;
+	var f = 1.0 / Math.tan(fovy / 2),
+		nf;
+	out[0] = f / aspect;
+	out[1] = 0;
+	out[2] = 0;
+	out[3] = 0;
+	out[4] = 0;
+	out[5] = f;
+	out[6] = 0;
+	out[7] = 0;
+	out[8] = 0;
+	out[9] = 0;
+	out[11] = -1;
+	out[12] = 0;
+	out[13] = 0;
+	out[15] = 0;
 
-    if (far != null && far !== Infinity) {
-      nf = 1 / (near - far);
-      out[10] = (far + near) * nf;
-      out[14] = 2 * far * near * nf;
-    } else {
-      out[10] = -1;
-      out[14] = -2 * near;
-    }
+	if (far != null && far !== Infinity) {
+		nf = 1 / (near - far);
+		out[10] = (far + near) * nf;
+		out[14] = 2 * far * near * nf;
+	} else {
+		out[10] = -1;
+		out[14] = -2 * near;
+	}
 	return out;
-  }
+}
 
 function lookAt(eye, center, up) {
 	var out;
-    var x0, x1, x2, y0, y1, y2, z0, z1, z2, len;
-    var eyex = eye[0];
-    var eyey = eye[1];
-    var eyez = eye[2];
-    var upx = up[0];
-    var upy = up[1];
-    var upz = up[2];
-    var centerx = center[0];
-    var centery = center[1];
-    var centerz = center[2];
+	var x0, x1, x2, y0, y1, y2, z0, z1, z2, len;
+	var eyex = eye[0];
+	var eyey = eye[1];
+	var eyez = eye[2];
+	var upx = up[0];
+	var upy = up[1];
+	var upz = up[2];
+	var centerx = center[0];
+	var centery = center[1];
+	var centerz = center[2];
 
-    z0 = eyex - centerx;
-    z1 = eyey - centery;
-    z2 = eyez - centerz;
-    len = 1 / Math.hypot(z0, z1, z2);
-    z0 *= len;
-    z1 *= len;
-    z2 *= len;
-    x0 = upy * z2 - upz * z1;
-    x1 = upz * z0 - upx * z2;
-    x2 = upx * z1 - upy * z0;
-    len = Math.hypot(x0, x1, x2);
+	z0 = eyex - centerx;
+	z1 = eyey - centery;
+	z2 = eyez - centerz;
+	len = 1 / Math.hypot(z0, z1, z2);
+	z0 *= len;
+	z1 *= len;
+	z2 *= len;
+	x0 = upy * z2 - upz * z1;
+	x1 = upz * z0 - upx * z2;
+	x2 = upx * z1 - upy * z0;
+	len = Math.hypot(x0, x1, x2);
 
-    if (!len) {
-      x0 = 0;
-      x1 = 0;
-      x2 = 0;
-    } else {
-      len = 1 / len;
-      x0 *= len;
-      x1 *= len;
-      x2 *= len;
-    }
+	if (!len) {
+		x0 = 0;
+		x1 = 0;
+		x2 = 0;
+	} else {
+		len = 1 / len;
+		x0 *= len;
+		x1 *= len;
+		x2 *= len;
+	}
 
-    y0 = z1 * x2 - z2 * x1;
-    y1 = z2 * x0 - z0 * x2;
-    y2 = z0 * x1 - z1 * x0;
-    len = Math.hypot(y0, y1, y2);
+	y0 = z1 * x2 - z2 * x1;
+	y1 = z2 * x0 - z0 * x2;
+	y2 = z0 * x1 - z1 * x0;
+	len = Math.hypot(y0, y1, y2);
 
-    if (!len) {
-      y0 = 0;
-      y1 = 0;
-      y2 = 0;
-    } else {
-      len = 1 / len;
-      y0 *= len;
-      y1 *= len;
-      y2 *= len;
-    }
+	if (!len) {
+		y0 = 0;
+		y1 = 0;
+		y2 = 0;
+	} else {
+		len = 1 / len;
+		y0 *= len;
+		y1 *= len;
+		y2 *= len;
+	}
 	out=[x0,y0,z0,0,x1,y1,z1,0,x2,y2,z2,0,-(x0 * eyex + x1 * eyey + x2 * eyez),-(y0 * eyex + y1 * eyey + y2 * eyez),-(z0 * eyex + z1 * eyey + z2 * eyez),1];
 	return out;
-  }
-  
+}
+
 function rotateX(m,a){
 	var c=Math.cos(a);
 	var s=Math.sin(a);
@@ -132,25 +129,25 @@ function makeCube(center,dimensions,rotations,rgb) {
 	var L = nVertices;
 	nVertices += 8;
 	vertices.push(center[0]-dimensions[0]/2,center[1]-dimensions[1]/2,center[2]-dimensions[2]/2,rgb[0],rgb[1],rgb[2],
-				center[0]+dimensions[0]/2,center[1]-dimensions[1]/2,center[2]-dimensions[2]/2,rgb[0],rgb[1],rgb[2],
-				center[0]+dimensions[0]/2,center[1]+dimensions[1]/2,center[2]-dimensions[2]/2,rgb[0],rgb[1],rgb[2],
-				center[0]-dimensions[0]/2,center[1]+dimensions[1]/2,center[2]-dimensions[2]/2,rgb[0],rgb[1],rgb[2],
-				center[0]-dimensions[0]/2,center[1]-dimensions[1]/2,center[2]+dimensions[2]/2,rgb[0],rgb[1],rgb[2],
-				center[0]+dimensions[0]/2,center[1]-dimensions[1]/2,center[2]+dimensions[2]/2,rgb[0],rgb[1],rgb[2],
-				center[0]+dimensions[0]/2,center[1]+dimensions[1]/2,center[2]+dimensions[2]/2,rgb[0],rgb[1],rgb[2],
-				center[0]-dimensions[0]/2,center[1]+dimensions[1]/2,center[2]+dimensions[2]/2,rgb[0],rgb[1],rgb[2]);
+		center[0]+dimensions[0]/2,center[1]-dimensions[1]/2,center[2]-dimensions[2]/2,rgb[0],rgb[1],rgb[2],
+		center[0]+dimensions[0]/2,center[1]+dimensions[1]/2,center[2]-dimensions[2]/2,rgb[0],rgb[1],rgb[2],
+		center[0]-dimensions[0]/2,center[1]+dimensions[1]/2,center[2]-dimensions[2]/2,rgb[0],rgb[1],rgb[2],
+		center[0]-dimensions[0]/2,center[1]-dimensions[1]/2,center[2]+dimensions[2]/2,rgb[0],rgb[1],rgb[2],
+		center[0]+dimensions[0]/2,center[1]-dimensions[1]/2,center[2]+dimensions[2]/2,rgb[0],rgb[1],rgb[2],
+		center[0]+dimensions[0]/2,center[1]+dimensions[1]/2,center[2]+dimensions[2]/2,rgb[0],rgb[1],rgb[2],
+		center[0]-dimensions[0]/2,center[1]+dimensions[1]/2,center[2]+dimensions[2]/2,rgb[0],rgb[1],rgb[2]);
 	indices.push(L+0, L+2, L+1,
-				L+0, L+3, L+2,
-				L+4, L+5, L+6,
-				L+4, L+6, L+7,
-				L+1, L+2, L+6,
-				L+1, L+6, L+5,
-				L+4, L+7, L+3,
-				L+4, L+3, L+0,
-				L+0, L+1, L+5,
-				L+0, L+5, L+4,
-				L+2, L+3, L+7,
-				L+2, L+7, L+6);
+		L+0, L+3, L+2,
+		L+4, L+5, L+6,
+		L+4, L+6, L+7,
+		L+1, L+2, L+6,
+		L+1, L+6, L+5,
+		L+4, L+7, L+3,
+		L+4, L+3, L+0,
+		L+0, L+1, L+5,
+		L+0, L+5, L+4,
+		L+2, L+3, L+7,
+		L+2, L+7, L+6);
 }
 
 function makeCustom(verts,inds) {
@@ -218,27 +215,26 @@ function pureGL(canvas) {
 	var color = gl.getAttribLocation(shaderProgram, 'color');
 	gl.vertexAttribPointer(color, 3, gl.FLOAT, false, 6 * Float32Array.BYTES_PER_ELEMENT, 3 * Float32Array.BYTES_PER_ELEMENT);
 	gl.enableVertexAttribArray(color);
-	
+
 	gl.enable(gl.DEPTH_TEST);
 	gl.viewport(0, 0, canvas.width, canvas.height);
-	gl.clearColor( 0, 0, 0, 1);
-	
+	gl.clearColor( 1, 1, 1, 1); // white
+
 	matWorldUniformLocation = gl.getUniformLocation(shaderProgram, 'mWorld');
 	var matViewUniformLocation = gl.getUniformLocation(shaderProgram, 'mView');
 	var matProjUniformLocation = gl.getUniformLocation(shaderProgram, 'mProj');
 
 	viewMatrix = new Float32Array(16);
 	projMatrix = new Float32Array(16);
-	
+
 	viewMatrix=lookAt([0,0,-100],[0,0,0],[0,1,0]);
 	projMatrix=perspective(Math.PI/4,canvas.clientWidth/canvas.clientHeight,0.1,1000.0);
 
 	gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
 	gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
-
 	a1=0;
 	a2=0;
-	
+
 	var drag=false;
 	var old_x,old_y;
 	var dX=0,dY=0;
@@ -252,7 +248,7 @@ function pureGL(canvas) {
 	var mouseMove=function(e){
 		if(!drag) return false;
 		dX=(e.pageX-old_x)*2*Math.PI/canvas.width,
-		dY=(e.pageY-old_y)*2*Math.PI/canvas.height;
+			dY=(e.pageY-old_y)*2*Math.PI/canvas.height;
 		a2+=dY;
 		if((Math.abs(a2)%(2*Math.PI)<(Math.PI/2))||(Math.abs(a2)%(2*Math.PI)>(3*Math.PI/2))){a1+=dX}
 		else{a1-=dX}
@@ -268,15 +264,13 @@ function pureGL(canvas) {
 	var touchMove=function(e){
 		if(!drag) return false;
 		dX=(e.changedTouches[0].pageX-old_x)*2*Math.PI/canvas.width,
-		dY=(e.changedTouches[0].pageY-old_y)*2*Math.PI/canvas.height;
+			dY=(e.changedTouches[0].pageY-old_y)*2*Math.PI/canvas.height;
 		a2+=dY;
 		if((Math.abs(a2)%(2*Math.PI)<(Math.PI/2))||(Math.abs(a2)%(2*Math.PI)>(3*Math.PI/2))){a1+=dX}
 		else{a1-=dX}
 		old_x=e.changedTouches[0].pageX,old_y=e.changedTouches[0].pageY;
 		e.preventDefault();
 	};
-	
-	
 	canvas.addEventListener("mousedown",mouseDown,false);
 	canvas.addEventListener("mouseup",mouseUp,false);
 	canvas.addEventListener("mouseout",mouseUp,false);
@@ -284,18 +278,15 @@ function pureGL(canvas) {
 	canvas.addEventListener("touchstart",touchDown,false);
 	canvas.addEventListener("touchend",mouseUp,false);
 	canvas.addEventListener("touchmove",touchMove,false);
-
-
 	requestAnimationFrame(loop);
 }
 
 function loop() {
-	
+
 	var mo_matrix=[1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1];
 	rotateY(mo_matrix,a1);
 	rotateX(mo_matrix,-a2);
 	gl.uniformMatrix4fv(matWorldUniformLocation,gl.FALSE,mo_matrix);
-	
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 	gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
 	requestAnimationFrame(loop);
